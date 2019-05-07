308de0309a0a3c8f126d316c7729c48c491c0930
724572275@qq.com <724572275@qq.com>
2019-05-07 14:17:22 +0800

    upadte index route

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports={
-    publicPath: process.env.NODE_ENV === 'production'? '/personal_blog/': '/', // 基本路径
+    publicPath: process.env.NODE_ENV === 'production'? '/personal_blog/dist': '/', // 基本路径
     pluginOptions:{
         CleanWebpackPlugin
     }
